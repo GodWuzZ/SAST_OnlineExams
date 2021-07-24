1b3b54023cea88ebbd4f7c82a775313a19fee35d
GodWuzZ <shermanfdy@gmail.com>
2021-07-24 20:09:15 +0800

    初始化

---
+++ src/main/java/sast/onlineexams/OnlineExamsApplication.java
@@ -0,0 +1,13 @@
+package sast.onlineexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sast/onlineexams/OnlineExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package sast.onlineexams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
