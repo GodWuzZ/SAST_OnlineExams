83d2c0b18381b9e072293fb1bcb9f7531a902406
GodWuzZ <shermanfdy@gmail.com>
2021-07-27 16:56:04 +0800

    用mbg生成model和mapper文件，增加了dto文件夹用于对象的传输

---
+++ src/main/java/sast/onlineexams/mbg/model/UmsAdmin.java
@@ -0,0 +1,61 @@
+package sast.onlineexams.mbg.model;
+
+import java.io.Serializable;
+
+public class UmsAdmin implements Serializable {
+    private Long id;
+
+    /**
+     * 用户名
+     *
+     * @mbg.generated
+     */
+    private String username;
+
+    /**
+     * 密码
+     *
+     * @mbg.generated
+     */
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
